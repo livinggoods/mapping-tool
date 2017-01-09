b791501e4067ffbc5af310dfa9a965882ea9e735
David Kimaru <kimarudg@gmail.com>
2017-01-09 07:27:08 +0300

    Create error pages

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,9 +4,11 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    page = {'title': 'Not Found', 'subtitle':'The page was not found'}
+    return render_template('page_404.html', page=page), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    page = {'title': 'Internal Error', 'subtitle':'Internal Error encountered'}
+    return render_template('page_500.html', page=page), 500
