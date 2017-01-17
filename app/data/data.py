e126fc61a382174cf25030ad7db750e15cc86a07
David Kimaru <kimarudg@gmail.com>
2017-01-17 12:50:24 +0300

    Add location data as a module

---
+++ app/data/data.py
@@ -0,0 +1,21 @@
+import csv, os
+
+def get_locations():
+  with open('data.csv', 'r') as f:
+    data_numbers = {}
+    districts = {}
+    counties = {}
+    sub_counties = {}
+    for row in csv.reader(f.read().splitlines()):
+      data_numbers[row[3]]= {'county':row[2], 'district':row[1], 'number':row[0]}
+      if row[1] in districts:
+        if row[2] in districts.get(row[1]):
+          districts[row[1]][row[2]].append({'name':row[3], 'number':row[0]})
+        else:
+          districts[row[1]][row[2]] = []
+          districts[row[1]][row[2]].append({'name':row[3], 'number':row[0]})
+      else:
+        districts[row[1]] = {}
+        districts[row[1]][row[2]] = []
+        districts[row[1]][row[2]].append({'name':row[3], 'number':row[0]})
+  return districts
