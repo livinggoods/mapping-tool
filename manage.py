a55d141021fdff1b1f857406c170979b7072faea
David Kimaru <kimarudg@gmail.com>
2017-01-17 12:49:44 +0300

    import locations

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from app import create_app, db
 from app.models import User, Role, Geo, UserType, Location
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from app.data import data
 
 application = create_app(os.getenv('FLASK_CONFIG', 'default'))
 manager = Manager(application)
