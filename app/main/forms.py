4cbf101cd61b73364b9868b37483445060c779ed
David Kimaru <kimarudg@gmail.com>
2017-01-19 16:07:50 +0300

    Transition from `Required()` to `DataRequired()`

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField, BooleanField, SelectField, \
     SubmitField, ValidationError
-from wtforms.validators import Required, Length, Email, Regexp
+from wtforms.validators import DataRequired, Length, Email, Regexp
 from ..models import Role, User, Geo, UserType
 
 
@@ -29,10 +29,10 @@ class EditProfileForm(Form):
 
 
 class EditProfileAdminForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     username = StringField('Username', validators=[
-        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                           'Usernames must have only letters, '
                                           'numbers, dots or underscores')])
     confirmed = BooleanField('Confirmed')
@@ -83,5 +83,5 @@ class EditProfileAdminForm(Form):
 
 
 class PostForm(Form):
-    body = TextAreaField("What's on your mind?", validators=[Required()])
+    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
