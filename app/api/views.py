5241a77984d71d52dd0738a03445d42271548581
David Kimaru <kimarudg@gmail.com>
2017-01-26 12:46:08 +0300

    Import DB for API app

--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,12 @@
 from . import api
 from flask_login import login_required, current_user
-from flask import Response
+from flask import Response, request, jsonify
 import json
+from sqlalchemy import func, distinct, select, and_
+from .. import db
+from ..models import (Permission, Role, User, Geo, UserType, Village, LocationTargets,
+    Location, Education, EducationLevel, Referral, InterviewScore, Chp, Recruitments,
+    SelectedApplication, Application, ApplicationPhone, Branch, Cohort)
 
 
 @api.route('/firm_summary')
@@ -9,3 +14,16 @@ import json
 def firm_summary():
     summary = current_user.firm_summary()
     return Response(json.dumps(summary, indent=4), mimetype='application/json')
+
+@api.route('/recruitments.json')
+def recruitments_json():
+  if request.method == 'GET':
+    page={'title':'Recruitments', 'subtitle':'Recruitments done so far'}
+    recruitments = Recruitments.query.filter_by(archived=0)
+    result_dict = [u.name for u in recruitments]
+    return jsonify(recruitments=result_dict)
+  else:
+    recruitments = Recruitments(name=request.form.get('name'))
+    db.session.add(recruitments)
+    db.session.commit()
+    return jsonify(status='ok')
