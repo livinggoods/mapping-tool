25adca0e127248dd6f5071fce7b2b7bae2f96d8e
David Kimaru <kimarudg@gmail.com>
2017-04-12 13:31:22 +0300

    Create API endpoints for the App

--- app/api/views.py
+++ app/api/views.py
@@ -39,3 +39,36 @@ def api_recruitment(id):
     db.session.add(recruitments)
     db.session.commit()
     return jsonify(status='ok')
+
+@api.route('/users/json', methods=['GET'])
+def api_users():
+  users = User.query.all()
+  api_data = []
+  for user in users:
+    api_data.append({'id':user.id, 'name':user.name, 'country':user.location, 'username': user.username, 'app_name': user.app_name, 'email': user.email})
+  return jsonify(users=api_data)
+
+@api.route('/test/json', methods=['GET', 'POST'])
+def get_test_url():
+  if request.method == 'GET':
+    return jsonify(status='get')
+  else:
+    return jsonify(status='post')
+
+@api.route('/sync/recruitments', methods=['GET', 'POST'])
+def sync_recruitments():
+  if request.method == 'GET':
+    records  = Recruitments.query.filter(Recruitments.archived != 1)
+    return jsonify({'recruitments':[record.to_json() for record in records]})
+  else:
+    status = []
+    recruitment_list = request.json.get('recruitments')
+    for recruitment in recruitment_list:
+      record = Recruitments.from_json(recruitment)
+      #exists = db.session.query(db.exists().where(User.name == 'davidism')).scalar()
+      if not db.session.query(db.exists().where(Recruitments.id == record.id)).scalar():
+         db.session.add(record)
+      status.append({'id':record.id, 'status':'ok'})
+    return jsonify(status=status)
+
+
