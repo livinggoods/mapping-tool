85d87dc9a21674b166cd8df098b8d4ff68fd6b44
David Kimaru <kimarudg@gmail.com>
2017-07-01 12:10:13 +0300

    Admins should be concerned with the Geo since this determines what users see

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,7 +7,7 @@ from ..models import Role, User, Geo, UserType
 
 class EditProfileForm(Form):
     name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
+    # location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     geo = SelectField('Geo', coerce=int)  # 'coerce': store values as ints
     user_type = SelectField('User Type', coerce=int)
@@ -31,14 +31,14 @@ class EditProfileForm(Form):
 class EditProfileAdminForm(Form):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                              Email()])
-    username = StringField('Username', validators=[
-        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                          'Usernames must have only letters, '
-                                          'numbers, dots or underscores')])
+    #username = StringField('Username', validators=[
+    #    DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+    #                                      'Usernames must have only letters, '
+    #                                      'numbers, dots or underscores')])
     confirmed = BooleanField('Confirmed')
     role = SelectField('Role', coerce=int)  # 'coerce': store values as ints
     name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
+    # location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     geo = SelectField('Geo', coerce=int)  # 'coerce': store values as ints
     user_type = SelectField('User Type', coerce=int)
