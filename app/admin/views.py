d5d50e6f8bed56df66b1de2fc3753250e0fc75ba
David Kimaru <kimarudg@gmail.com>
2017-08-17 21:06:59 +0300

    Admin dashboard

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,10 +1,18 @@
 from flask import render_template, redirect, url_for, flash, request, abort, \
     current_app, jsonify, make_response
 from flask_login import current_user, login_required
-from ..models import User, Geo
+from ..models import User, Geo, IccmComponents
+from ..main.forms import IccmComponentForm
+from ..decorators import admin_required, permission_required
 from .. import db
 from . import admin
+import time
 
+
+@admin.route('/', methods=['GET'])
+@login_required
+def admin_dashboard():
+    return make_response(jsonify(status='dashboard coming soon'), 200)
 
 @admin.route('/users', methods=['GET'])
 @login_required
