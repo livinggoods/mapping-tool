70e3e940214670e9849f261a59b8fec3a4b26c65
David Kimaru <kimarudg@gmail.com>
2017-09-19 07:33:30 +0300

    Return Registration chew Details as JSON

--- app/models.py
+++ app/models.py
@@ -489,7 +489,9 @@ class Registration (db.Model):
             'parish' : self.parish,
             'financial_accounts' : self.financial_accounts,
             'recruitment_transport' : self.recruitment_transport,
-            'branch_transport' : self.branch_transport
+            'branch_transport' : self.branch_transport,
+            'referral_id' : self.referral_id,
+            'referral_details' : self.chew_referral.to_json(),
             }
 
         return json_record
