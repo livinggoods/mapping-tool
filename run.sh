373ca8f9405aca5c28d8d873113c20ea66b6493b
David Kimaru <kimarudg@gmail.com>
2018-03-23 09:38:45 +0300

    Refactor Gunicorn logging

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-gunicorn -b 0.0.0.0:5000 --workers 8 --access-logformat "%(h)s %(r)s %(s)s"  --access-logfile - --error-logfile - wsgi:application
+gunicorn -b 0.0.0.0:5000 --workers 8 --access-logformat "Remote client - %({X-Forwarded-For}i)s Webserver - %(h)s %(r)s %(s)s"  --access-logfile logs/expansion.log --error-logfile - wsgi:application
